ecb071b6b0e322f59679165c5b7aed0edf3e604d
Radhika Aggarwal <radhika142006@gmail.com>
2025-12-13 00:51:39 +0530

    Implement basic arithmetic operations with input
    Fix input prompts and output formatting for clarity.

---
+++ Day1que2.py
@@ -0,0 +1,15 @@
+'''Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
+Sample Test Cases:
+Input 1:
+10 2
+Output 1:
+Sum=12, Diff=8, Product=20, Quotient=5
+
+Input 2:
+7 3
+Output 2:
+Sum=10, Diff=4, Product=21, Quotient=2'''
+
+n1=int(input("enter a number:"))
+n2=int(input("enter another numnber:"))
+print("sum=",n1+n2," diff=",n1-n2," product=",n1*n2," quotient=",n1 // n2)
