b3de3ec6f4fa082e41cb3de352e66d01a19b21ba
Radhika Aggarwal <radhika142006@gmail.com>
2025-12-13 23:30:41 +0530

    Add temperature conversion from Celsius to Fahrenheit
    Implement Celsius to Fahrenheit conversion program.

---
+++ Day3que1.py
@@ -0,0 +1,16 @@
+'''Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212'''
+
+c=int(input("enter the temperature in celsius: "))
+d=9/5
+f=(d*c)+32
+print("Farenheit= ",f)
