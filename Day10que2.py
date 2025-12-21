10e70009b9a7ec34caaf156442cbf8df9ad46b12
Radhika Aggarwal <radhika142006@gmail.com>
2025-12-21 23:25:46 +0530

    Add switch-case program for day of the week
    Implement a program to display the day of the week based on user input.

---
+++ Day10que2.py
@@ -0,0 +1,29 @@
+'''Q20: Write a program to display the day of the week based on a number (1–7) using switch-case.
+
+/*
+Sample Test Cases:
+Input 1:
+1
+Output 1:
+Monday
+
+Input 2:
+5
+Output 2:
+Friday'''
+n=int(input("enter num: "))
+match n:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
